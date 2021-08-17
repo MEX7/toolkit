abe8a66c070bff4f774bc0c493fe307ccc4b4848
kl7sn <mex7.0828@gmail.com>
2021-08-17 17:45:40 +0800

    feat: redis pub/set
    Signed-off-by: kl7sn <mex7.0828@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kl7sn/toolkit
 go 1.16
 
 require (
-	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/go-redis/redis/v8 v8.11.3
+	github.com/mailru/easyjson v0.7.7
 	github.com/segmentio/kafka-go v0.4.17
 )
---
+++ kredis/init.go
@@ -0,0 +1,62 @@
+package kredis
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var Subscribe *subscribe
+
+type subscribe struct {
+	DbRedis  *redis.Client
+	channels map[string]*redis.PubSub
+}
+
+// Init 全局 Subscribe 初始化
+func Init(ctx context.Context, client *redis.Client, channels map[string]func(ctx context.Context, pubSub *redis.PubSub)) {
+	Subscribe = &subscribe{
+		DbRedis:  client,
+		channels: make(map[string]*redis.PubSub),
+	}
+	for channel, fc := range channels {
+		ps := Subscribe.Sub(ctx, channel)
+		Subscribe.channels[channel] = ps
+		go fc(ctx, ps)
+	}
+}
+
+// Close 消息订阅关闭
+func Close() error {
+	if Subscribe == nil {
+		return nil
+	}
+	for _, pubSub := range Subscribe.channels {
+		_ = pubSub.Close()
+	}
+	_ = Subscribe.DbRedis.Close()
+	return nil
+}
+
+// Sub 订阅
+func (s *subscribe) Sub(ctx context.Context, channel string) *redis.PubSub {
+	var err error
+	pubSub := s.DbRedis.Subscribe(ctx, channel)
+	_, err = pubSub.Receive(ctx)
+	if err != nil {
+		return nil
+	}
+	return pubSub
+}
+
+// Pub 发布
+func (s *subscribe) Pub(ctx context.Context, channel string, message []byte) error {
+	var err error
+	err = s.DbRedis.Publish(ctx, channel, message).Err()
+	if err != nil {
+		fmt.Printf("subscribe error: %s", err.Error())
+		return err
+	}
+	return nil
+}
