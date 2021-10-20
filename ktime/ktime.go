70c55636341d993040b10d63b5bae1c0b9feb5a8
杜旻翔 <duminxiang@douyu.tv>
2021-10-20 14:24:54 +0800

    feat: math
    Signed-off-by: 杜旻翔 <duminxiang@douyu.tv>

--- ktime/ktime.go
+++ ktime/ktime.go
@@ -9,6 +9,10 @@ func Millisecond() int64 {
 	return time.Now().UnixNano() / 1e6
 }
 
+func Microsecond() int64 {
+	return time.Now().UnixNano() / 1e3
+}
+
 func Cost(t time.Time, label ...string) {
 	tc := time.Since(t)
 	if len(label) > 0 {
