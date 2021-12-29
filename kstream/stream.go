b0d1d954d1425e33dff36dff27efde81b127a711
kl7sn <mex7.0828@gmail.com>
2021-12-29 16:26:59 +0800

    fix: 提供read可选开关
    Signed-off-by: kl7sn <mex7.0828@gmail.com>

--- kstream/stream.go
+++ kstream/stream.go
@@ -28,42 +28,43 @@ type ProxyStream struct {
 	msgChan chan *pb.CellReq
 }
 
-func InitStream(client pb.StreamClient, callback Callback) *ProxyStream {
+func InitStream(client pb.StreamClient, callback Callback, isRead bool) *ProxyStream {
 	obj := &ProxyStream{msgChan: make(chan *pb.CellReq, 1000)}
 	obj.PushChan(&pb.CellReq{MsgId: MsgConnectReq})
-	obj.sync(client, callback)
+	obj.sync(client, callback, isRead)
 	return obj
 }
 
 func (p *ProxyStream) PushChan(info *pb.CellReq) {
 	p.msgChan <- info
 }
-
-func (p *ProxyStream) sync(client pb.StreamClient, callback Callback) {
-	xgo.Go(func() {
-		var (
-			reply *pb.CellResp
-			err   error
-		)
-		for {
-			reply, err = p.GetStream(client).Recv()
-			if err != nil {
-				replyStatus, _ := status.FromError(err)
-				if replyStatus.Code() == codes.Unavailable {
-					fmt.Println("与服务器的连接被断开, 进行重试")
-					fmt.Println("Receive reply error:", err.Error())
-					p.DelStream()
+func (p *ProxyStream) sync(client pb.StreamClient, callback Callback, isRead bool) {
+	if isRead {
+		xgo.Go(func() {
+			var (
+				reply *pb.CellResp
+				err   error
+			)
+			for {
+				reply, err = p.GetStream(client).Recv()
+				if err != nil {
+					replyStatus, _ := status.FromError(err)
+					if replyStatus.Code() == codes.Unavailable {
+						fmt.Println("与服务器的连接被断开, 进行重试")
+						fmt.Println("Receive reply error:", err.Error())
+						p.DelStream()
+						continue
+					}
 					continue
 				}
-				continue
+				if reply.Code != 0 {
+					fmt.Println("Receive reply code is not 0:", reply.Code)
+					continue
+				}
+				callback(reply)
 			}
-			if reply.Code != 0 {
-				fmt.Println("Receive reply code is not 0:", reply.Code)
-				continue
-			}
-			callback(reply)
-		}
-	})
+		})
+	}
 
 	xgo.Go(func() {
 		var err error
